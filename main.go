b325bd18e11d3d2ba5500afc4d35f0e20b3e510d
Jelte Dirks <j.dirks@outlook.com>
2022-05-25 22:13:42 +0200

    reorder
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
     "strconv"
 )
 
-var toplevel int = 200 // lower case is package private
-var TOPLEVEL int = 300 // upper case is public
+var toplevel int = 200 // first letter is lower case: package private
+var TOPLEVEL int = 300 // first letter is upper case: public
 
 var (
     name string = "Jelte"
@@ -14,25 +14,23 @@ var (
     major string = "CS"
 )
 
-func main() { // block scoped variables inside func
+func main() { 
+    // block scoped variables inside func
     var thisisfloat32 float32 = 10
     var thisisfloat64 = 10.
     var thisisint = 10
-
-    var name string = "this is okay" // this is not: name := "this is not okay"
-
-    fmt.Println(name)
-
 	fmt.Printf("%v %T \n", thisisint, thisisint)
 	fmt.Printf("%v %T \n", thisisfloat32, thisisfloat32)
 	fmt.Printf("%v %T \n", thisisfloat64, thisisfloat64)
     fmt.Printf("%v %T \n", toplevel, toplevel)
 
+    var name string = "this is okay" 
+    fmt.Println(name)
+    // this is not: name := "this is not okay"
+
     var start_as_int int = 4523
     var should_be_float float32
-
     should_be_float = float32(start_as_int)
-
     fmt.Printf("%v %T \n", should_be_float, should_be_float)
 
     var smiley int = 128512 // int -> string conversion takes unicode character
